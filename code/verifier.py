8e59fac9e726cf5dfa110c70829b6d7fdaf94a84
Harish Rajagopal <harish.rajagopals@gmail.com>
2021-11-15 20:19:41 +0100

    Formatted verifier.py with black and isort

--- code/verifier.py
+++ code/verifier.py
@@ -1,8 +1,9 @@
 import argparse
+
 import torch
 from networks import FullyConnected
 
-DEVICE = 'cpu'
+DEVICE = "cpu"
 INPUT_SIZE = 28
 
 
@@ -11,45 +12,61 @@ def analyze(net, inputs, eps, true_label):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Neural network verification using DeepPoly relaxation')
-    parser.add_argument('--net',
-                        type=str,
-                        required=True,
-                        help='Neural network architecture which is supposed to be verified.')
-    parser.add_argument('--spec', type=str, required=True, help='Test case to verify.')
+    parser = argparse.ArgumentParser(
+        description="Neural network verification using DeepPoly relaxation"
+    )
+    parser.add_argument(
+        "--net",
+        type=str,
+        required=True,
+        help="Neural network architecture which is supposed to be verified.",
+    )
+    parser.add_argument(
+        "--spec", type=str, required=True, help="Test case to verify."
+    )
     args = parser.parse_args()
 
-    with open(args.spec, 'r') as f:
+    with open(args.spec, "r") as f:
         lines = [line[:-1] for line in f.readlines()]
         true_label = int(lines[0])
         pixel_values = [float(line) for line in lines[1:]]
-        eps = float(args.spec[:-4].split('/')[-1].split('_')[-1])
+        eps = float(args.spec[:-4].split("/")[-1].split("_")[-1])
 
-    if args.net.endswith('fc1'):
+    if args.net.endswith("fc1"):
         net = FullyConnected(DEVICE, INPUT_SIZE, [50, 10]).to(DEVICE)
-    elif args.net.endswith('fc2'):
+    elif args.net.endswith("fc2"):
         net = FullyConnected(DEVICE, INPUT_SIZE, [100, 50, 10]).to(DEVICE)
-    elif args.net.endswith('fc3'):
+    elif args.net.endswith("fc3"):
         net = FullyConnected(DEVICE, INPUT_SIZE, [100, 100, 10]).to(DEVICE)
-    elif args.net.endswith('fc4'):
+    elif args.net.endswith("fc4"):
         net = FullyConnected(DEVICE, INPUT_SIZE, [100, 100, 50, 10]).to(DEVICE)
-    elif args.net.endswith('fc5'):
-        net = FullyConnected(DEVICE, INPUT_SIZE, [100, 100, 100, 100, 10]).to(DEVICE)
+    elif args.net.endswith("fc5"):
+        net = FullyConnected(DEVICE, INPUT_SIZE, [100, 100, 100, 100, 10]).to(
+            DEVICE
+        )
     else:
         assert False
 
-    net.load_state_dict(torch.load('../mnist_nets/%s.pt' % args.net, map_location=torch.device(DEVICE)))
+    net.load_state_dict(
+        torch.load(
+            "../mnist_nets/%s.pt" % args.net, map_location=torch.device(DEVICE)
+        )
+    )
 
-    inputs = torch.FloatTensor(pixel_values).view(1, 1, INPUT_SIZE, INPUT_SIZE).to(DEVICE)
+    inputs = (
+        torch.FloatTensor(pixel_values)
+        .view(1, 1, INPUT_SIZE, INPUT_SIZE)
+        .to(DEVICE)
+    )
     outs = net(inputs)
     pred_label = outs.max(dim=1)[1].item()
     assert pred_label == true_label
 
     if analyze(net, inputs, eps, true_label):
-        print('verified')
+        print("verified")
     else:
-        print('not verified')
+        print("not verified")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
